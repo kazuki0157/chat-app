fd373e62e8b50489fe257837fc8a602f464262b8
kazuki0157 <apokali0157@gmail.com>
2022-04-12 16:53:07 +0900

    メッセージ送信機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,7 @@
 class RoomsController < ApplicationController
 
   def index
+
   end
   
   def new
@@ -21,4 +22,5 @@ class RoomsController < ApplicationController
   def room_params
     params.require(:room).permit(:name, user_ids: [])
   end
+
 end
